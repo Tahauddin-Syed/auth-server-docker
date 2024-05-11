93e5d8c1ccb8e16f802428db9121df1040e0b57f
Tahauddin1990 <syedsdroid@gmail.com>
2024-05-11 17:30:57 +0300

    removed security filter chain bean

--- src/main/java/com/tahauddin/syed/configurration/SecurityConfiguration.java
+++ src/main/java/com/tahauddin/syed/configurration/SecurityConfiguration.java
@@ -1,18 +1,15 @@
 package com.tahauddin.syed.configurration;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 
 @Configuration
 @Slf4j
 public class SecurityConfiguration {
+
+
+
 
 
 
