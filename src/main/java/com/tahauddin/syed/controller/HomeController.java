04ddd32960279031dddd851e4c5eebb56c9fa756
Tahauddin1990 <syedsdroid@gmail.com>
2024-05-11 18:29:57 +0300

    Added Security Configuration

--- src/main/java/com/tahauddin/syed/controller/HomeController.java
+++ src/main/java/com/tahauddin/syed/controller/HomeController.java
@@ -3,13 +3,14 @@ package com.tahauddin.syed.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -30,6 +31,12 @@ public class HomeController {
     public ResponseEntity getHomePage1 (@CurrentSecurityContext(expression = "authentication.name") String name) {
         log.info("Home1 Page Called ");
         log.info("Principal Object is :: {}", name);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity getHomePage1 (@RequestBody Map<String, String> request) {
+
         return ResponseEntity.ok().build();
     }
 
